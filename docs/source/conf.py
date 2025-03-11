e77445209dae7a96c0a65c50741fb91b4ea38db2
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-03-11 16:21:47 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,3 +57,13 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+html_static_path = ['_static']
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+html_js_files = [
+    'js/custom.js',
+]
+
