67cb2521ee3129b11e33d22ad58460e5486fc883
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-03-11 15:34:13 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': '#2B4C65',
+    'style_nav_header_background': '#36454F',
 
     # Toc options
     'collapse_navigation': True,
