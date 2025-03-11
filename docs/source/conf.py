473349e04bbb63a0d10008b73f19823e593b8ddd
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-03-11 16:09:13 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'De Jure'
+copyright = '2025, Mac Kelly'
+author = 'Mac Kelly'
 
 release = '0.1'
 version = '0.1.0'
