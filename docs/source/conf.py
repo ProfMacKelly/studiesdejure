4228247632807b0f4a35701b7e2a9edb38a6dc6a
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-03-11 14:30:39 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,3 +42,20 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_theme_options = {
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    'flyout_display': 'hidden',
+    'version_selector': True,
+    'language_selector': True,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
