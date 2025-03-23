6b22dd8773acec173b70e277a43035b06a8b8532
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-03-23 16:32:37 -0500

    Update extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,3 +1,15 @@
+function toggleText(element) {
+    let hiddenText = element.previousElementSibling;
+    
+    if (hiddenText.style.display === "none" || hiddenText.style.display === "") {
+        hiddenText.style.display = "inline";
+        element.textContent = " ⬅"; // Change ellipsis to collapse symbol
+    } else {
+        hiddenText.style.display = "none";
+        element.textContent = ". . ."; // Restore ellipsis
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const abbrElements = document.querySelectorAll('abbr');
 
